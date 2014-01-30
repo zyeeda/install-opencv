855a066aac527bb11cba39647c5f55493d19f3dd
sgjava <sgjava@gmail.com>
2014-01-30 11:24:51 -0500

    Handle VideoCapture.open always returning True with bad URLs or V4L devices

--- opencv-python/codeferm/CaptureUI.py
+++ opencv-python/codeferm/CaptureUI.py
@@ -5,7 +5,7 @@ Created by Steven P. Goldsmith on December 21, 2013
 sgoldsmith@codeferm.com
 """
 
-import logging, sys, re, cv2, cv2.cv as cv
+import logging, sys, re, socket, cv2, cv2.cv as cv
 
 """A simple video capture script using imshow.
 
@@ -30,18 +30,26 @@ elif re.match(r"[-+]?\d+$", sys.argv[1]) is not None:
     url = int(sys.argv[1])
 else:
     url = sys.argv[1]
-videoCapture = cv2.VideoCapture(url)
+# Set socket timeout in seconds (defaults to forever)
+socket.setdefaulttimeout(10)    
+videoCapture = cv2.VideoCapture()
+# This returns True even for bad URLs
+success = videoCapture.open(url)
 logger.info("OpenCV %s" % cv2.__version__)
 logger.info("Press [Esc] to exit")
 logger.info("URL: %s" % url)
 logger.info("Resolution: %dx%d" % (videoCapture.get(cv.CV_CAP_PROP_FRAME_WIDTH),
                                videoCapture.get(cv.CV_CAP_PROP_FRAME_HEIGHT)))
-cv2.namedWindow("Python Capture")
-key = -1
-# Wait for escape to be pressed
-while(key < 0):
-    videoCapture.grab()
-    success, image = videoCapture.read()
-    cv2.imshow("Python Capture", image)
-    key = cv2.waitKey(1)
-cv2.destroyAllWindows()
+# Deal with VideoCapture always returning True otherwise it will hang on VideoCapture.grab()
+if videoCapture.get(cv.CV_CAP_PROP_FRAME_WIDTH) > 0 and videoCapture.get(cv.CV_CAP_PROP_FRAME_HEIGHT) > 0:
+    cv2.namedWindow("Python Capture")
+    key = -1
+    # Wait for escape to be pressed
+    while(key < 0 and success):
+        videoCapture.grab()
+        success, image = videoCapture.read()
+        cv2.imshow("Python Capture", image)
+        key = cv2.waitKey(1)
+    cv2.destroyAllWindows()
+else:
+    logger.error("Unable to open device")
