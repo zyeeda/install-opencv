f1e673154d0958ac610e6fd13dca61f426685d8d
sgjava <sgjava@gmail.com>
2014-05-15 13:18:35 -0400

    Updated JDK version

--- scripts/ubuntu/config.sh
+++ scripts/ubuntu/config.sh
@@ -33,15 +33,15 @@ installjava="True"
 
 # Oracle JDK
 if [ $installjava = "True" ]; then
-	jdkurl="http://download.oracle.com/otn-pub/java/jdk/8-b132/"
+	jdkurl="http://download.oracle.com/otn-pub/java/jdk/8u5-b13/"
 	jdkver="jdk1.8.0"
 	javahome=/usr/lib/jvm/jdk1.8.0
 	if [ "$arch" = "x86_64" ]; then
-		jdkarchive="jdk-8-linux-x64.tar.gz"
+		jdkarchive="jdk-8u5-linux-x64.tar.gz"
 	elif [ "$arch" = "i586" ] || [ "$arch" = "i686" ]; then
-		jdkarchive="jdk-8-linux-i586.tar.gz"
+		jdkarchive="jdk-8u5-linux-i586.tar.gz"
 	elif [ "$arch" = "armv7l" ]; then
-		jdkarchive="jdk-8-linux-arm-vfp-hflt.tar.gz"
+		jdkarchive="jdk-8u5-linux-arm-vfp-hflt.tar.gz"
 	else
 		echo "\nNo supported architectures detected!"
 		exit 1
